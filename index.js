8365ab46358982adb1fd629955e9a9651ff8a1da
Olusola Afikode <afikodeolaitan@gmail.com>
2021-09-25 16:20:30 +0100

    Add date, time and media queries for screens

--- index.js
+++ index.js
@@ -36,5 +36,40 @@ pushBtn.addEventListener('click', () => {
     inputTask.value = "";
 })
 
+function date__time(id) {
+    date = new Date;
+    year = date.getFullYear();
+    month = date.getMonth();
+    months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    d = date.getDate();
+    day = date.getDay();
+    days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    
+    //get hours
+    h = date.getHours();
+    if(h < 10) {
+        h = "0" + h;
+    }
+
+    //get minutes
+    m = date.getMinutes();
+    if(m < 10) {
+        m = "0" + m;
+    }
+
+    //get seconds
+    s = date.getSeconds();
+    if(s < 10) {
+        s = "0" + s;
+    }
+
+    result = '' + days[day] + ' ' + months[month] + ' ' + d + ' ' + year + ' ' + h + ':' + m + ':' + s;
+    document.getElementById(id).innerHTML = result;
+    setTimeout('date__time("'+id+'");' ,'1000');
+    return true;
+}
+
+window.onload=date__time("date__time")
+
 
 
